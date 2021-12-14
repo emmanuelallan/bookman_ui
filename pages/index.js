c2a676bca9c11d17669cbb9682a589d1aae869c3
maxthestranger <thestrangermax@gmail.com>
2021-12-14 10:07:13 +0300

    feat: WIP login

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,52 @@
 import Main from '../sections/main';
 
-export default function Home() {
-  return <Main>Login</Main>;
+import Image from 'next/image';
+
+export default function Login() {
+  return (
+    <Main>
+      <div className="container d_flex">
+        <div className="row">
+          <div className="col_5">
+            <div className="card">
+              <div className="card_body p_6">
+                <div className="mb_4">
+                  <div className="mb_4">
+                    <Image
+                      src="/img/bookman_store_logo.svg"
+                      alt="bookman logo"
+                      width={200}
+                      height={50}
+                    />
+                  </div>
+                  <h1 className="mb_1 fw_bold">Sign In</h1>
+                </div>
+
+                <form>
+                  <div className="mb_3">
+                    <label className="form_label">Username or Email</label>
+                    <input
+                      type="email"
+                      className="form_control"
+                      name="email"
+                      placeholder="Email address here"
+                      required
+                    />
+                  </div>
+
+                  <div>
+                    <div className="d_grid">
+                      <button type="submit" className="btn_submit btn_primary">
+                        Publish
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Main>
+  );
 }
---
+++ pages/login.js
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+
+export default function Login() {
+  return (
+    <div className="container d_flex">
+      <div className="row">
+        <div className="col_5">
+          <div className="card">
+            <div className="card_body p_6">
+              <div className="mb_4">
+                <div className="mb_4">
+                  <Image
+                    src="/img/bookman_store_logo.svg"
+                    alt="bookman logo"
+                    width={200}
+                    height={50}
+                  />
+                </div>
+                <h1 className="mb_1 fw_bold">Sign In</h1>
+              </div>
+
+              <form>
+                <div className="mb_3">
+                  <label className="form_label">Username or Email</label>
+                  <input
+                    type="email"
+                    className="form_control"
+                    name="email"
+                    placeholder="Email address here"
+                    required
+                  />
+                </div>
+
+                <div>
+                  <div className="d_grid">
+                    <button type="submit" className="btn btn_primary">
+                      Sign In
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
